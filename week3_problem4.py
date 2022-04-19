1869b96504052c585ef60c5f85170690d29305d8
sravaniboggaram <boggaram.sravani@gmail.com>
2022-04-18 23:08:04 -0700

    Add files via upload

---
+++ week3_problem4.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+#
+#   motordemo.py
+#
+#   This shows how to interface with the GPIO (general purpose I/O)
+#   pins and how to drive the PWM for the motors.  Please use as an
+#   example, but change to suit the weekly goals.
+#
+# Imports
+from turtle import left
+import pigpio
+import sys
+import time
+# Define the motor pins.
+MTR1_LEGA = 7
+MTR1_LEGB = 8
+MTR2_LEGA = 5
+MTR2_LEGB = 6
+
+class Motor:
+
+    def __init__(self):
+        # Initialize the connection to the pigpio daemon (GPIO interface).
+        self.io = pigpio.pi()
+        if not self.io.connected:
+            print("Unable to connection to pigpio daemon!")
+            sys.exit(0)
+
+        # Clear all pins, just in case.
+        self.io.set_PWM_dutycycle(MTR1_LEGA, 0)
+        self.io.set_PWM_dutycycle(MTR1_LEGB, 0)
+        self.io.set_PWM_dutycycle(MTR2_LEGA, 0)
+        self.io.set_PWM_dutycycle(MTR2_LEGB, 0)
+        print("GPIO ready...")
+
+
+    def shutdown(self):
+        # Clear all pins, just in case.
+        print("Turning off")
+        self.io.set_PWM_dutycycle(MTR1_LEGA, 0)
+        self.io.set_PWM_dutycycle(MTR1_LEGB, 0)
+        self.io.set_PWM_dutycycle(MTR2_LEGA, 0)
+        self.io.set_PWM_dutycycle(MTR2_LEGB, 0)
+
+        self.io.stop()
+
+    def set(self, leftdutycycle, rightdutycycle):
+
+        # Set up the four pins as output (commanding the motors).
+        self.io.set_mode(MTR1_LEGA, pigpio.OUTPUT)
+        self.io.set_mode(MTR1_LEGB, pigpio.OUTPUT)
+        self.io.set_mode(MTR2_LEGA, pigpio.OUTPUT)
+        self.io.set_mode(MTR2_LEGB, pigpio.OUTPUT)
+
+        # Prepare the PWM.  The range gives the maximum value for 100%
+        # duty cycle, using integer commands (1 up to max).
+        self.io.set_PWM_range(MTR1_LEGA, 255)
+        self.io.set_PWM_range(MTR1_LEGB, 255)
+        self.io.set_PWM_range(MTR2_LEGA, 255)
+        self.io.set_PWM_range(MTR2_LEGB, 255)
+        
+        # Set the PWM frequency to 1000Hz.  You could try 500Hz or 2000Hz
+        # to see whether there is a difference?
+        self.io.set_PWM_frequency(MTR1_LEGA, 1000)
+        self.io.set_PWM_frequency(MTR1_LEGB, 1000)
+        self.io.set_PWM_frequency(MTR2_LEGA, 1000)
+        self.io.set_PWM_frequency(MTR2_LEGB, 1000)
+
+        if leftdutycycle < 0:
+            self.io.set_PWM_dutycycle(MTR1_LEGA, 0)
+            self.io.set_PWM_dutycycle(MTR1_LEGB, leftdutycycle*255)
+        else:
+            self.io.set_PWM_dutycycle(MTR1_LEGA, leftdutycycle*255)
+            self.io.set_PWM_dutycycle(MTR1_LEGB, 0)
+
+        if rightdutycycle < 0:
+            self.io.set_PWM_dutycycle(MTR2_LEGA, 0)
+            self.io.set_PWM_dutycycle(MTR2_LEGB, rightdutycycle*255)
+        else:
+            self.io.set_PWM_dutycycle(MTR2_LEGA, rightdutycycle*255)
+            self.io.set_PWM_dutycycle(MTR2_LEGB, 0)
+        
+
+#
+#   Main
+#
+if __name__ == "__main__":
+    
+    motor = Motor()
+    motor.set(0.60, 0.60)
+    motor.shutdown()
+    
